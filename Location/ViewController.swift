181f7443f3acecb4398506f0ded3e8bc12113505
Vibhor Chaudhary <vibhor@moveinsync.com>
2020-04-14 02:45:52 +0530

    Initial Commit

---
+++ Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Location
+//
+//  Created by Vibhor Chaudhary on 14/04/20.
+//  Copyright © 2020 Vibhor Chaudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
